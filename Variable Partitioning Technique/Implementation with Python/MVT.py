b4f2417bfa726d7bb42a3e6aa532ab2609f6eae8
jelhamm <elhamjafari200312@gmail.com>
2023-11-04 15:57:33 +0330

    Add VariablePartition class

---
+++ Variable Partitioning Technique/Implementation with Python/MVT.py
@@ -0,0 +1,22 @@
+# The following code simulates a memory manager that manages variable memory partitions.
+# Processes can allocate and deallocate memory blocks from this manager using the MVT algorithm.
+
+
+class VariablePartition:
+    def __init__(self, total_memory):
+        self.total_memory = total_memory
+        self.partitions = []
+        self.memory_map = {}
+
+    def add_partition(self, start_address, size):
+        self.partitions.append((start_address, size))
+
+    def display_memory_map(self):
+        print("Memory Map:")
+        if not self.memory_map:
+            print("No allocated memory.")
+        else:
+            for process_id, (start_address, size) in self.memory_map.items():
+                print("Process", process_id, ": Start Address", start_address, ", Size", size)
+        print("")
+  
