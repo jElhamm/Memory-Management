938f435f2ccc522895ddef7c3fd46fa2dcada5d3
jelhamm <elhamjafari200312@gmail.com>
2023-11-04 15:06:46 +0330

    Display Output

--- Fixed Partitioning Technique/Implementation with Python/MFT.py
+++ Fixed Partitioning Technique/Implementation with Python/MFT.py
@@ -35,3 +35,45 @@ class FixedPartition:
                 print("Block", i + 1, ": Process", self.memory_map[i])
         print("")
  
+
+
+# Display Output
+
+def banner():
+    print("""
+          
+********************************************************************************************************
+*                          Welcome to the FixedPartition Memory Manager!                               *
+*                                                                                                      *
+*         This program simulates a memory manager that manages a fixed memory partition with           *
+*     blocks of equal size. Processes can allocate and deallocate memory blocks from this manager.     *
+*                                                                                                      *
+*       To get started, please provide the following information:                                      *
+*       1. Total available memory                                                                      *
+*       2. Block size                                                                                  *
+*       3. Number of blocks in memory                                                                  *
+*       4. Number of processes                                                                         *
+*                                                                                                      *      
+********************************************************************************************************
+    """)
+
+def main():
+    banner()
+    total_memory = int(input("---> Enter total available memory: "))
+    block_size = int(input("---> Enter block size: "))
+    num_blocks = int(input("---> Enter number of blocks in memory: "))
+    memory_manager = FixedPartition(total_memory, block_size, num_blocks)
+    num_processes = int(input("---> Enter number of processes: "))
+    for i in range(num_processes):
+        process_id = i + 1
+        memory_required = int(input("---> Enter memory required for process {}: ".format(process_id)))
+        if memory_manager.allocate_memory(process_id, memory_required):
+            print("Memory allocated for process", process_id)
+        else:
+            print("Memory allocation failed for process", process_id)
+    
+    memory_manager.display_memory_map()
+
+
+if __name__ == '__main__':
+    main()
