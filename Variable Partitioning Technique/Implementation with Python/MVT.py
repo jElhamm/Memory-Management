12995be1ee79607c98ac5cff26042893dc47663b
jelhamm <elhamjafari200312@gmail.com>
2023-11-04 16:00:12 +0330

    Implemented memory allocation function

--- Variable Partitioning Technique/Implementation with Python/MVT.py
+++ Variable Partitioning Technique/Implementation with Python/MVT.py
@@ -11,6 +11,17 @@ class VariablePartition:
     def add_partition(self, start_address, size):
         self.partitions.append((start_address, size))
 
+    def allocate_memory(self, process_id, memory_required):
+        for i, partition in enumerate(self.partitions):
+            start_address, size = partition
+            if size >= memory_required:
+                # Allocate memory for the process
+                self.memory_map[process_id] = (start_address, memory_required)
+                # Update the partition with the remaining memory
+                self.partitions[i] = (start_address + memory_required, size - memory_required)
+                return True
+        return False
+    
     def display_memory_map(self):
         print("Memory Map:")
         if not self.memory_map:
