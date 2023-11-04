0f32cedbd117bd926c2159114dd4e3f468f15eea
jelhamm <elhamjafari200312@gmail.com>
2023-11-04 16:05:37 +0330

    Display Output

--- Variable Partitioning Technique/Implementation with Python/MVT.py
+++ Variable Partitioning Technique/Implementation with Python/MVT.py
@@ -42,3 +42,51 @@ class VariablePartition:
                 print("Process", process_id, ": Start Address", start_address, ", Size", size)
         print("")
   
+
+
+# Display Output
+
+def banner():
+    print("""
+          
+************************************************************************************************
+*                         Welcome to the VariablePartition Memory Manager!                     *
+*                                                                                              *
+*       This program simulates a memory manager that manages variable memory partitions.       *
+*       Processes can allocate and deallocate memory blocks from this manager using MVT.       *
+*                                                                                              *
+*       To get started, please provide the following information:                              *
+*       1. Total available memory                                                              *
+*       2. Number of partitions                                                                *
+*       3. Details of each partition (start address and size)                                  *
+*       4. Number of processes                                                                 *
+*       5. Memory requirement for each process                                                 *
+*                                                                                              *      
+************************************************************************************************
+    """)
+
+def main():
+    banner()
+    total_memory = int(input("---> Enter total available memory: "))
+    memory_manager = VariablePartition(total_memory)
+    #******************************************************************************************
+    num_partitions = int(input("---> Enter number of partitions: "))
+    for i in range(num_partitions):
+        start_address = int(input("---> Enter start address for partition {}: ".format(i+1)))
+        size = int(input("---> Enter size for partition {}: ".format(i+1)))
+        memory_manager.add_partition(start_address, size)
+    #******************************************************************************************
+    num_processes = int(input("---> Enter number of processes: "))
+    for i in range(num_processes):
+        process_id = i + 1
+        memory_required = int(input("---> Enter memory required for process {}: ".format(process_id)))
+        if memory_manager.allocate_memory(process_id, memory_required):
+            print("Memory allocated for process", process_id)
+        else:
+            print("Memory allocation failed for process", process_id)
+    #******************************************************************************************
+    memory_manager.display_memory_map()
+
+
+if __name__ == '__main__':
+    main()
